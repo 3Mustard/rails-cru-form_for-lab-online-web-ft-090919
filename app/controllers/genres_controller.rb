2018a371fbeb407a6e826697a3298ededf7e9202
Matthew C <m23.cullen@gmail.com>
2019-11-12 22:41:42 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,38 @@
 class GenresController < ApplicationController
+  
+  def index
+  end 
+  
+  def new 
+  end 
+  
+  def create 
+  end 
+  
+  def show 
+  end 
+  
+  def edit 
+  end 
+  
+  def update 
+  end 
+  
+  private 
+  
+  def get_genre
+    @genre = Genre.find(params[:id])
+  end 
+  
+  def genre_params
+    params.require(:genre).permit(:)
+  end 
 end
+
+
+
+
+
+
+
+
