1b2bec5ea52c598be630c4e762ef83fdbd2d2462
Matthew C <m23.cullen@gmail.com>
2019-11-12 23:17:40 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,25 +1,39 @@
 class SongsController < ApplicationController
   
   def index 
+    @songs = Song.all
   end 
   
   def new 
+    @song = Song.new
   end 
   
-  def create 
+  def create
+     @song = Song.new(song_params)
+     @song.save
+     redirect_to song_path(@song) 
   end 
   
   def show 
+    get_song
   end 
   
   def edit 
+    get_song
   end 
   
-  def update 
+  def update
+    get_song
+    @song.update(song_params)
+    redirect_to song_path(@song)
   end 
   
   private 
   
   def get_song
+    @song = Song.find(params[:id])
+  end 
+  
+  def song_params 
     params.require(:song).permit(:name)
 end
